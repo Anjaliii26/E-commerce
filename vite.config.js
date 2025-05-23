976ce32134dee099457b43335df48ae55ba20d92
Anjaliii26 <Anjaliiiii26@gmail.com>
2025-05-23 17:54:34 +0530

    Add basename to BrowserRouter

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/E-commerce/', 
+  base: '/E-commerce/',
   plugins: [
     tailwindcss(),
     react()
   ],
+  server: {
+    hmr: {
+      path: '/ws',  // or try just true if this causes issues
+    },
+  },
 })
